d1edf4b6cd4ce6eac1119683772988329836bc2b
Stephane Wartel <stephane.wartel@deveryware.com>
2016-11-06 18:04:21 +0100

    Fix chmod

--- add-jenkins-user.sh
+++ add-jenkins-user.sh
@@ -3,9 +3,7 @@
 echo "Creating user jenkins"
 useradd jenkins \
   && passwd -d  jenkins \
-  && mkdir -p /home/jenkins/.ssh \
   && mkdir -p /home/jenkins/backups \
-  && chown -R jenkins:jenkins /home/jenkins \
   && adduser jenkins sudo
 
 if [ -z "${SSH_KEY}" ]; then
@@ -14,7 +12,10 @@ if [ -z "${SSH_KEY}" ]; then
 fi
 
 echo "Adding your ssh public key to user jenkins"
+mkdir -p /home/jenkins/.ssh && \
+  && chmod go-rwx /home/jenkins/.ssh
 echo "${SSH_KEY}" > /home/jenkins/.ssh/authorized_keys \
   && chmod go-rw /home/jenkins/.ssh/authorized_keys
+chown -R jenkins:jenkins /home/jenkins
 
 echo "You can now connect to your container using user jenkins"
